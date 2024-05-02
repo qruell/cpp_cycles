b6c3465dec64b3cab64d217d4b6ddfd85a52f37e
qruell <illya.goncharuk.angelok@gmail.com>
2024-05-02 19:20:14 +0300

    ex1

---
+++ Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    int a;
+    std::cout << "Enter value a: ";
+    std::cin >> a;
+
+    int sum = 0;
+    for (int i = a; i <= 500; ++i) {
+        sum += i;
+    }
+
+    std::cout << "Sum of integers from " << a << " to 500: " << sum << std::endl;
+
+    return 0;
+}
