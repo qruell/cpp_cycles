99de531134408df16cc11eee62729ed47a638efb
qruell <illya.goncharuk.angelok@gmail.com>
2024-05-02 19:38:43 +0300

    ex2

--- Source.cpp
+++ Source.cpp
@@ -1,16 +1,46 @@
 #include <iostream>
-#include <cmath>
 
 int main() {
-    int x, y;
-    std::cout << "Enter the number x: ";
-    std::cin >> x;
-    std::cout << "Enter the number y (degree): ";
-    std::cin >> y;
+    int start, end;
 
-    double result = pow(x, y);
+    std::cout << "Enter the range (two numbers separated by space): ";
+    std::cin >> start >> end;
 
-    std::cout << x << " in degree " << y << " = " << result << std::endl;
+    if (start > end) {
+        int temp = start;
+        start = end;
+        end = temp;
+    }
+
+    std::cout << "All numbers in the range [" << start << ", " << end << "]:" << std::endl;
+    for (int i = start; i <= end; ++i) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Even numbers in the range [" << start << ", " << end << "]:" << std::endl;
+    for (int i = start; i <= end; ++i) {
+        if (i % 2 == 0) {
+            std::cout << i << " ";
+        }
+    }
+    std::cout << std::endl;
+
+    std::cout << "Odd numbers in the range [" << start << ", " << end << "]:" << std::endl;
+    for (int i = start; i <= end; ++i) {
+        if (i % 2 != 0) {
+            std::cout << i << " ";
+        }
+    }
+    std::cout << std::endl;
+
+    std::cout << "Numbers divisible by seven in the range [" << start << ", " << end << "]:" << std::endl;
+    for (int i = start; i <= end; ++i) {
+        if (i % 7 == 0) {
+            std::cout << i << " ";
+        }
+    }
+    std::cout << std::endl;
 
     return 0;
 }
