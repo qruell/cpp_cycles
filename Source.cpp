b847149141706931c74a43b811586a976694c844
qruell <illya.goncharuk.angelok@gmail.com>
2024-05-02 20:32:29 +0300

    ex1

--- Source.cpp
+++ Source.cpp
@@ -2,12 +2,14 @@
 
 int main() {
     int a;
-    std::cout << "Enter the value of 'a': ";
+    std::cout << "Enter a value for 'a': ";
     std::cin >> a;
 
     int sum = 0;
-    for (int i = a; i <= 500; ++i) {
-        sum += i;
+    int current = a;
+    while (current <= 500) {
+        sum += current;
+        current++;
     }
 
     std::cout << "Sum of integers from " << a << " to 500: " << sum << std::endl;
